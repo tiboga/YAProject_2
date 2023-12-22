e87cca346333ee6d026906939f922d3459b7fd29
TIBO <bor_tim@list.ru>
2023-12-22 22:04:30 +0300

    demo 1.0.1

--- main.py
+++ main.py
@@ -3,60 +3,12 @@ import sys
 
 import pygame
 
-FPS = 50
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-
-    image = pygame.image.load(fullname)
-    image
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
+FPS = 100
 pygame.init()
 size = WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode(size)
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-clock = pygame.time.Clock()
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('main_hero.png')
-
-tile_width = tile_height = 50
+speed_x = 4
+change = (0,0)
 
 
 class Tile(pygame.sprite.Sprite):
@@ -68,7 +20,7 @@ class Tile(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x=WIDTH//2, pos_y=HEIGHT//2):
+    def __init__(self, pos_x=WIDTH // 2, pos_y=HEIGHT // 2):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
@@ -81,46 +33,58 @@ class Player(pygame.sprite.Sprite):
 
 
 class Camera:
-    # зададим начальный сдвиг камеры
     def __init__(self):
         self.dx = 0
         self.dy = 0
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
         obj.rect.y += self.dy
 
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+    def update(self, player):
+        global change
+        if change[0] != 0 or change[1] != 0:
+            self.dx = -change[0] * 1.5
+            self.dy = -change[1] * 1.5
+        change = (0, 0)
+        if self.dx != 0:
+            print(self.dx, self.dy)
 
 
-player = None
 
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
 
-level_map = load_level('map.txt')
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 def move(player, movement):
+    global change
     x, y = player.pos
+    # if movement == 'jump':
     if movement == 'up':
         if y > 50:
-            player.move(x, y - 10)
+            change = (0, -speed_x)
     elif movement == 'down':
         if y < level_y - 90:
-            player.move(x, y + 10)
+            change = (0, speed_x)
     elif movement == 'left':
         if x > 50:
-            player.move(x - 10, y)
+            change = (-speed_x, 0)
     elif movement == 'right':
         if x < level_x - 90:
-            player.move(x + 10, y)
+            change = (speed_x, 0)
 
 
 def generate_level(level):
@@ -132,9 +96,43 @@ def generate_level(level):
             elif level[y][x] == '#':
                 Tile('wall', x, y)
     new_player = Player(250, 250)
+    new_player.move(250, 250)
     # вернем игрока, а также размер поля в клетках
     return new_player, WIDTH, HEIGHT
 
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+clock = pygame.time.Clock()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('texture_fon.png')
+}
+player_image = load_image('Woodcutter.png')
+
+tile_width = tile_height = 50
+
+player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+level_map = load_level('map.txt')
 
 player, level_x, level_y = generate_level(load_level('map.txt'))
 
@@ -173,7 +171,24 @@ if __name__ == '__main__':
     counter = 0
     running = True
     current_press = ''
+    jump = False
     while running:
+        if current_press == 'w':
+            if counter == 2:
+                move(player, 'up')
+                counter = 0
+        if current_press == 's':
+            if counter == 2:
+                move(player, 'down')
+                counter = 0
+        if current_press == 'a':
+            if counter == 2:
+                move(player, 'left')
+                counter = 0
+        if current_press == 'd':
+            if counter == 2:
+                move(player, 'right')
+                counter = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -181,52 +196,45 @@ if __name__ == '__main__':
                 if event.key == pygame.K_w:
                     move(player, 'up')
                     current_press = 'w'
-                    Counter_ticks = True
                     counter = 0
+                    Counter_ticks = True
                 elif event.key == pygame.K_s:
                     move(player, 'down')
                     current_press = 's'
-                    Counter_ticks = True
                     counter = 0
+                    Counter_ticks = True
                 elif event.key == pygame.K_a:
                     move(player, 'left')
                     current_press = 'a'
-                    Counter_ticks = True
                     counter = 0
+                    Counter_ticks = True
                 elif event.key == pygame.K_d:
                     move(player, 'right')
                     current_press = 'd'
-                    Counter_ticks = True
                     counter = 0
+                    Counter_ticks = True
+
+                elif event.key == pygame.K_SPACE:
+                    move(player, 'jump', speed=20)
+
             elif event.type == pygame.KEYUP:
                 current_press = ''
-                Counter_ticks = False
                 counter = 0
-            if current_press == 'w':
-                if counter == 2:
-                    move(player, 'up')
-                    counter = 0
-            if current_press == 's':
-                if counter == 2:
-                    move(player, 'down')
-                    counter = 0
-            if current_press == 'a':
-                if counter == 2:
-                    move(player, 'left')
-                    counter = 0
-            if current_press == 'd':
-                if counter == 2 :
-                    move(player, 'right')
-                    counter = 0
-            if Counter_ticks:
-                counter += 1
-        # # изменяем ракурс камеры
-        # camera.update(player)
-        # # обновляем положение всех спрайтов
-        # for sprite in all_sprites:
-        #     camera.apply(sprite)
+                Counter_ticks = False
+        if Counter_ticks:
+            counter += 1
+
+        # изменяем ракурс камеры
+        camera.update(player)
+        # обновляем положение всех спрайтов
+        for sprite in tiles_group:
+            camera.apply(sprite)
+        camera.dx = 0
+        camera.dy = 0
         screen.fill('black')
         tiles_group.draw(screen)
         player_group.draw(screen)
         pygame.display.flip()
         clock.tick(100)
+        if change[0] != 0:
+            print(change)
